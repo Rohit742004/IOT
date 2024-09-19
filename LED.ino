594dc3ce45d82987a38f67f215f0a55d758d9eae
Rohit Badi <157134069+Rohit742004@users.noreply.github.com>
2024-09-19 18:54:32 +0530

    Create LED.ino

---
+++ LED.ino
@@ -0,0 +1,10 @@
+//It glows light of bulb evey second.
+void setup() {
+  pinMode(13, OUTPUT);
+}
+void loop() {
+  digitalWrite(13, HIGH);   
+  delay(1000);              
+  digitalWrite(13, LOW);   
+  delay(1000);             
+}
